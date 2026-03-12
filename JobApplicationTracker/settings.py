2393327df7152f6f0c32de3eec984781235d0f2e
VitorBressan <contato.vbressan@gmail.com>
2026-03-12 10:21:08 -0300

    add: add postgres connection

--- JobApplicationTracker/settings.py
+++ JobApplicationTracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'job_applications',
 ]
 
 MIDDLEWARE = [
@@ -71,11 +73,17 @@ WSGI_APPLICATION = 'JobApplicationTracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'JobApplicationTracker',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
