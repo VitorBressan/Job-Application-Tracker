4de23db94e456e1ec67a7822065f50fe0a94b86c
VitorBressan <contato.vbressan@gmail.com>
2026-03-16 11:49:01 -0300

    add: add models to the admin page

--- job_applications/admin.py
+++ job_applications/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 from .models import Application, ApplicationEvent
 
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ApplicationEvent)
+class ApplcationEventAdmin(admin.ModelAdmin):
+    pass
+
