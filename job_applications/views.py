d9c1e34aecf5aee07c883ca046854b16071395cf
VitorBressan <contato.vbressan@gmail.com>
2026-03-30 18:10:31 -0300

    add: add job application registration

--- job_applications/views.py
+++ job_applications/views.py
@@ -1,7 +1,10 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from .models import *
+from django.urls import reverse_lazy
 from .forms import ApplicationRegisterForm  
 
 @login_required
@@ -11,3 +14,10 @@ def home(request):
 class AddApplicationView(CreateView):
     form_class = ApplicationRegisterForm
     template_name = 'job_applications/add_application.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        application_object = form.save(commit=False)
+        application_object.user = self.request.user
+        application_object.save()
+        return super().form_valid(form)
