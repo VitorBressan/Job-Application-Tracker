730292cef377a6f644a27a4f9482cc1fa5e89cd4
VitorBressan <contato.vbressan@gmail.com>
2026-04-01 15:38:42 -0300

    improve the design

--- job_applications/forms.py
+++ job_applications/forms.py
@@ -6,7 +6,7 @@ class ApplicationRegisterForm(ModelForm):
         model = Application
         fields = ['company_name', 'role', 'vacancy_link', 'company_address','status', 'date_applied', 'notes', 'salary_min', 'salary_max', 'salary_period', 'currency', 'work_mode']
         help_texts = {
-            'company_adress': 'Optional',
+            'company_address': 'Optional',
             'notes': 'Optional',
             'salary_min': 'Fill min and max salary with the same amount if the salary is fix (Optional)',
             'salary_max': 'Fill min and max salary with the same amount if the salary is fix (Optional)',
