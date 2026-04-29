750a44f1b1ab2248ea322a4f889816fa3bbb3e81
VitorBressan <contato.vbressan@gmail.com>
2026-04-29 16:56:37 -0300

    ui: improves list of applications on the home page

--- job_applications/forms.py
+++ job_applications/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class ApplicationForm(ModelForm):
     class Meta:
         model = Application
-        fields = ['company_name', 'role', 'vacancy_link', 'company_address','status', 'date_applied', 'notes', 'salary_min', 'salary_max', 'salary_period', 'currency', 'work_mode']
+        fields = ['company_name', 'role', 'vacancy_link', 'status', 'work_mode', 'contract_type', 'company_address', 'date_applied', 'notes', 'salary_min', 'salary_max', 'salary_period', 'currency', ]
         help_texts = {
             'company_address': 'Optional',
             'notes': 'Optional',
@@ -12,7 +12,8 @@ class ApplicationForm(ModelForm):
             'salary_max': 'Fill min and max salary with the same amount if the salary is fix (Optional)',
             'salary_period': 'Optional',
             'currency': 'Optional',
-            'work_mode': 'Optional'
+            'work_mode': 'Optional',
+            'contract_type': 'Optional'
         }
 
 class EventForm(ModelForm):
@@ -22,3 +23,5 @@ class EventForm(ModelForm):
         widgets = {
             'event_date': DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
+        
+        
