b08d66c2fa97aead7fe8b5421c694cb07aff180f
VitorBressan <contato.vbressan@gmail.com>
2026-04-30 14:32:30 -0300

    ui: improved the application details page design

--- job_applications/forms.py
+++ job_applications/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class ApplicationForm(ModelForm):
     class Meta:
         model = Application
-        fields = ['company_name', 'role', 'vacancy_link', 'status', 'work_mode', 'contract_type', 'company_address', 'date_applied', 'notes', 'salary_min', 'salary_max', 'salary_period', 'currency', ]
+        fields = ['company_name', 'role', 'vacancy_link', 'status', 'vacancy_description','work_mode', 'contract_type', 'company_address', 'date_applied', 'notes', 'salary_min', 'salary_max', 'salary_period', 'currency', ]
         help_texts = {
             'company_address': 'Optional',
             'notes': 'Optional',
@@ -13,7 +13,8 @@ class ApplicationForm(ModelForm):
             'salary_period': 'Optional',
             'currency': 'Optional',
             'work_mode': 'Optional',
-            'contract_type': 'Optional'
+            'contract_type': 'Optional',
+            'vacancy_description': 'Copy and paste de vacancy description here (Optional)'
         }
 
 class EventForm(ModelForm):
